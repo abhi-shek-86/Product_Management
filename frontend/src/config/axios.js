353e388f5b8d5d495e5aa1ea8c66a8f390651424
ABHISHEK <rapoabhi86@gmail.com>
2025-03-20 14:38:31 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default instance;
